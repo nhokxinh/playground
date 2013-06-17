ed24ccf0e076ccf43c696df07f160f049602b93d
longlc <leconglong@gmail.com>
2013-06-17 23:36:28 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'playground');
 
 /** MySQL database username */
-define('DB_USER', 'longlc');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'longlc');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
